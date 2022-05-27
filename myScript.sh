47efc730fa6552165c08d61f60e5079b2a635883
Omar I. Martínez Gómez <omar_238@hotmail.com>
2022-05-27 15:30:11 -0500

    modificación del comando que llama al script borrar... para ejecutarlo con ruta absoluta

--- myScript.sh
+++ myScript.sh
@@ -1,2 +1,2 @@
-alias borrarCacheSpotify="sh borrarCacheSpotify.sh"
+alias borrarCacheSpotify="sh /Users/omar/Projects/scripts/borrarCacheSpotify.sh"
 alias cdphp="cd /Users/omar/.bitnami/stackman/machines/xampp/volumes/root/htdocs/curso_php"
