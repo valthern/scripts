ebe68f794189176108ee321f309657198e1e8f82
Omar I. Martínez Gómez <omar_238@hotmail.com>
2023-03-07 21:53:48 -0600

    adición de shibangs en cada archivo

--- borrarCacheSpotify.sh
+++ borrarCacheSpotify.sh
@@ -1,3 +1,4 @@
+#! /bin/bash
 echo 'Se borrará la cache de Spotify'
 rm -rf "/Users/omar/Library/Application Support/Spotify/PersistentCache/Storage/*"
 echo 'La caché de Spotify ha sido borrada'
--- myScript.sh
+++ myScript.sh
@@ -1,2 +1,3 @@
+#! /bin/bash
 alias borrarCacheSpotify="sh /Users/omar/Projects/scripts/borrarCacheSpotify.sh"
 alias cdphp="cd /Users/omar/.bitnami/stackman/machines/xampp/volumes/root/htdocs/curso_php"
