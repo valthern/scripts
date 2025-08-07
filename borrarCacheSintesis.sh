f0c3c0d84604b4ff7d0f93e0e76e1e62223284d6
Omar I. Martínez Gómez <omar_238@hotmail.com>
2025-08-06 21:57:22 -0600

    Actualizacion de borrarCacheSintesis.sh

--- borrarCacheSintesis.sh
+++ borrarCacheSintesis.sh
@@ -1,9 +1,22 @@
 #!/usr/bin/env bash
 
+borrar_cache() {
+    local ruta="$1"
+    rm -rf "$ruta"
+    if [[ $? -eq 0 ]]; then
+        echo "✅ Borrado exitoso: $ruta"
+    else
+        echo "❌ Error al borrar: $ruta"
+    fi
+}
+
 echo 'Se borrará la cache de Sintesis'
-rm -rf /home/httpsarmandoprid/public_html/wp-content/cache/supercache/*
-rm -rf /home/adminrostros/public_html/wp-content/cache/object/*
-rm -rf /home/sntesiscom/public_html/wp-content/cache/object/*
-rm -rf /home/sntesiscom/public_html/puebla/wp-content/cache/object/*
-rm -rf /home/sntesiscom/public_html/tlaxcala/wp-content/cache/object/*
+
+borrar_cache "/home/httpsarmandoprid/public_html/wp-content/cache/supercache/*" &
+borrar_cache "/home/adminrostros/public_html/wp-content/cache/object/*" &
+borrar_cache "/home/sntesiscom/public_html/wp-content/cache/object/*" &
+borrar_cache "/home/sntesiscom/public_html/puebla/wp-content/cache/object/*" &
+borrar_cache "/home/sntesiscom/public_html/tlaxcala/wp-content/cache/object/*" &
+wait
+
 echo 'La caché de Sintesis ha sido borrada'
