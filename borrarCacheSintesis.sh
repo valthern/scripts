2ed1be45035a573b987c18620cb047ca1e4e7520
Omar I. Martínez Gómez <omar_238@hotmail.com>
2025-08-06 21:43:57 -0600

    Se agrego el script para borrar la cache de Sintesis

---
+++ borrarCacheSintesis.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+echo 'Se borrará la cache de Sintesis'
+rm -rf /home/httpsarmandoprid/public_html/wp-content/cache/supercache/*
+rm -rf /home/adminrostros/public_html/wp-content/cache/object/*
+rm -rf /home/sntesiscom/public_html/wp-content/cache/object/*
+rm -rf /home/sntesiscom/public_html/puebla/wp-content/cache/object/*
+rm -rf /home/sntesiscom/public_html/tlaxcala/wp-content/cache/object/*
+echo 'La caché de Sintesis ha sido borrada'
