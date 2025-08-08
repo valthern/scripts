dee274db490fb8ec14add5fb422455c0fa29be23
Omar I. Martínez Gómez <omar_238@hotmail.com>
2025-08-08 10:40:31 -0600

    Desliegue de Inodos y espacio en disco en el script  de Síntesis

--- borrarCacheSintesis.sh
+++ borrarCacheSintesis.sh
@@ -2,11 +2,19 @@
 # Script to clear cache for Sintesis websites
 echo 'Se borrará la cache de los sitios de Síntesis'
 
+echo 'Inodos:'
+df -i
+echo 'Espacio en disco:'
+df -h
 rm -rf /home/httpsarmandoprid/public_html/wp-content/cache/supercache/* &
 rm -rf /home/adminrostros/public_html/wp-content/cache/object/* &
 rm -rf /home/sntesiscom/public_html/wp-content/cache/object/* &
 rm -rf /home/sntesiscom/public_html/puebla/wp-content/cache/object/* &
 rm -rf /home/sntesiscom/public_html/tlaxcala/wp-content/cache/object/* &
 wait
+echo 'Inodos:'
+df -i
+echo 'Espacio en disco:'
+df -h
 
 echo 'La caché de Sintesis ha sido borrada'
